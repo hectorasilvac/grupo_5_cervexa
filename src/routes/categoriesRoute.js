f207d6dc6169062974afd4ce8ddca8c442b62428
silvahector <hascardenas@gmail.com>
2021-05-22 15:10:00 -0500

    Se corrige el controlador de Categorías

--- src/routes/categoriesRoute.js
+++ src/routes/categoriesRoute.js
@@ -8,21 +8,6 @@ router.post('/', categoriesController.create);
 router.get('/search', categoriesController.search);
 router.get('/:id', categoriesController.findByID);
 router.put('/:id', categoriesController.update);
-// router.delete('/:id', categoriesController.delete);
-
-// // const productsController = require('../controllers/productsController');
-// // const createValidation = require('../middlewares/products/validateCreateMiddleware');
-// // const upload = require('../middlewares/products/uploadImageMiddleware');
-// // const userLogged = require('../middlewares/users/userLoggedMiddleware');
-// // const adminMiddleware = require('../middlewares/users/adminMiddleware');
-
-// // router.get('/', adminMiddleware, productsController.showAll);
-// // router.get('/create', adminMiddleware, productsController.create);
-// // router.get('/:id', userLogged, productsController.details);
-// // router.get('/:id/edit', productsController.editById);
-// // router.delete('/:id', adminMiddleware, productsController.delete);
-// // router.post('/', upload.single('image'), createValidation, productsController.addRegister);
-// // /* Save updated product information */
-// // router.put('/:id', upload.single('image'), createValidation, productsController.save);
+router.delete('/:id', categoriesController.delete);
 
 module.exports = router;
